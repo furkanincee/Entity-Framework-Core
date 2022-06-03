15e3f6c8a025d11197a97d18c05248e287b64b26
furkanincee <76855467+furkanincee@users.noreply.github.com>
2022-06-03 13:42:24 +0300

    EF Core Yaklasimlar

---
+++ EF Core Yaklasimlar/EF Core Yaklasimlar/Program.cs
@@ -0,0 +1,13 @@
+﻿
+/*
+ * Önceden hazırlanmış bir veritabanı varsa --> DbFirst
+ * DbFirst yaklaşımı ile var olan veritabanını tersine mühendislik ile kod kısmında modelleriz.
+ * 
+ * Uzun süreli devam eden uygulamalarda (devlet projeleri gibi köklü kurumsal projelerin veritabanları modellenirken mesela)
+ * Veritabanı yönetim sürecine ve tasarımına dair kararların geliştiriciler tarafından alınmadığı durumlarda DbFirst tercih edilmelidir.
+ * DB'deki değişiklikler geliştirici tarafından kod tarafında kontrol edilip güncellenmelidir.
+ * 
+ * Veritabanı önceden oluşturulmamışsa veritabanının kod kısmında modellenip sonradan veritabanı sunucusuna migrate edilmesi --> CodeFirst
+ * 
+ * Yönetim süreci geliştirici tarafından sağlanır.
+ */
