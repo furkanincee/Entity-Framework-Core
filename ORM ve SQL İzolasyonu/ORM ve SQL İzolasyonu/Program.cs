13aac8bf8a2f068a9bf1fdbc2ac16dbc83bdfdd2
furkanincee <76855467+furkanincee@users.noreply.github.com>
2022-06-03 13:10:13 +0300

    ORM, SQL İzolasyonu, EF Kurulumu

--- ORM ve SQL İzolasyonu/ORM ve SQL İzolasyonu/Program.cs
+++ ORM ve SQL İzolasyonu/ORM ve SQL İzolasyonu/Program.cs
@@ -7,4 +7,13 @@
  * ORM teknolojileri yazılan sorguyu arka planda kullanılan database'in diline çevirerek gönderir.
  * 
  * SQL server yüklediğinde gelen SQL Profiler ile atılmış bir LINQ sorgusunun SQL çıktısını görebilirsin
+ * 
+ * dotnet-ef CLI komutlarını kullanabilmemiz için;
+ * Windows powershellden dotnet tool install --global dotnet-ef ile global olarak toolu yükleyebiliriz
+ * güncelleme için dotnet tool update --global dotnet-ef
+ * kontrol etmek için dotnet ef
+ * 
+ * Tool yüklendikten sonra bir projede kullanabilmek için Microsoft.EntityFrameworkCore.Design paketini projeye yüklememiz gerek. (CLI ile ilgili kısımlar için)
+ * 
+ * Package-manager üzerinden kullanımlar için Microsoft.EntityFrameworkCore.Tools
  */
