09d8228a5c28b691ab8bfb68e6d945d49bc02d35
furkanincee <76855467+furkanincee@users.noreply.github.com>
2022-06-03 12:31:37 +0300

    Advantages of ORM

---
+++ ORM ve SQL İzolasyonu/ORM ve SQL İzolasyonu/Program.cs
@@ -0,0 +1,10 @@
+﻿
+/* ORM teknolojileri OOP'nin özelliklerinden faydalanarak veri tabanının koda dökülmesini sağlar.
+ * Veritabanındaki tabloların sınıflar, satırların da objeler olarak gelmesi vb işlemlere genel olarak 
+ * object related mapping denir. Yani nesne ilgili eşleme.
+ * 
+ * Kodun yönetimini kolaylaştırır, kodu kullanılan database programından bağımsızlaştırır.
+ * ORM teknolojileri yazılan sorguyu arka planda kullanılan database'in diline çevirerek gönderir.
+ * 
+ * SQL server yüklediğinde gelen SQL Profiler ile atılmış bir LINQ sorgusunun SQL çıktısını görebilirsin
+ */
