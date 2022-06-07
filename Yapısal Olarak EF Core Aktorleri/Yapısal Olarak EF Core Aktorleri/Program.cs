dc2b22eb01b69a9c37fc4b5d0e4f4e6eda28b2b7
furkanincee <76855467+furkanincee@users.noreply.github.com>
2022-06-07 17:02:26 +0300

    DbContext Kavramı

---
+++ Yapısal Olarak EF Core Aktorleri/Yapısal Olarak EF Core Aktorleri/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Veri tabanını temsil edecek olan class --> DbContexttir. 
+
+public class myContext : DbContext // bu şekilde dbcontext sınıfından türetilmelidir burada hata gözüküyor tabi çünkü ef toollarını kurmadım
+{
+    public DbSet<Employee> Employees { get; set; } 
+    // mimaride entity olan her şey veritabanın içinde gösterilir.
+    // Burada aslında şöyle birşey demiş oluyoruz içinde employee'ler barındıran bir employees tablosu var 
+}
+
+// DbContext sınıfı;
+// *Konfigürasyon -> Veritabanı bağlantısı vs ilişkileri sağlar.
+// *Sorgulama -> Kod tarafında gerçekleştirilen sorguları SQL sorgusuna dönüştürerek veritabanına gönderir
+// *Change Tracking -> Veriler üzerindeki değişiklikleri takip eder.
+// *Veri kalıcılığı -> Verilerin kaydı güncellenmesi silinmesi operasyonlarını gerçekleştirir
+// *Caching
+
+// EF Core'da tabloları temsil eden sınıflar 'Entity' olarak isimlendirilir. 
