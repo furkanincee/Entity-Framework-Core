ef5f8c5d0699c31df95bb2c2a8689d680f75fe37
furkanincee <76855467+furkanincee@users.noreply.github.com>
2022-07-07 19:15:54 +0300

    OnConfiguring Metodu

---
+++ OnConfiguring ve Entity Tanımlama Kuralları/OnConfiguring ve Entity Tanımlama Kuralları/ETicaretContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnConfiguring_ve_Entity_Tanımlama_Kuralları
+{
+    public class ETicaretContext : DbContext
+    {
+        public DbSet<Urun> Urunler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // override edilerek kullanılır
+        {
+            optionsBuilder.UseSqlServer("");
+            // Provider, Connection String, Lazy Loading vb yapılandırmalar yapılır
+        }
+    }
+
+    public class Urun
+    {
+        public int Id { get; set; }
+
+    }
+
+    #region Entity Tanımlama Kuralları
+    // Her tablonun default olarak bir primary key kolonu olması gerekir. (HasNoKey kullanarak bunun üstesinden gelinebiliyor)
+    // Idli bir property varsa EF onu otomatik olarak primary key olarak alır.
+    #endregion
+}
---
+++ OnConfiguring ve Entity Tanımlama Kuralları/OnConfiguring ve Entity Tanımlama Kuralları/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnConfiguring_ve_Entity_Tanımlama_Kuralları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }    
+}
